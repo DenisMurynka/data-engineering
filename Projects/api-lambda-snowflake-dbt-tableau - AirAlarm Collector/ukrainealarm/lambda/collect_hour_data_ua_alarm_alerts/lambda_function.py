d5a7b83109a2eb9f8a3fd58ce890cdc8b2fd147c
Denis the f <dmurinka@gmail.com>
2024-07-03 20:22:36 +0300

    added config file

--- Projects/api-lambda-snowflake-dbt-tableau - AirAlarm Collector/ukrainealarm/lambda/collect_hour_data_ua_alarm_alerts/lambda_function.py
+++ Projects/api-lambda-snowflake-dbt-tableau - AirAlarm Collector/ukrainealarm/lambda/collect_hour_data_ua_alarm_alerts/lambda_function.py
@@ -1,20 +1,38 @@
 import json
 import boto3
 import urllib3
+import configparser
+from datetime import datetime
+
+from configparser import ConfigParser
+
+
 
 def lambda_handler(event, context):
+    
+    dt = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+
+    config = ConfigParser()
+    configFilePath = "config.cfg"
+    with open(configFilePath) as f:
+        config.read_file(f)
+    API_KEY = config.get('credentials', 'api_key')
+
+    
     bucket_name = 'ua-alarms-stage'
-    file_name = 'file_name.json'
+    file_name = f'file_name_{dt}.json'
     s3 = boto3.resource('s3')
     obj = s3.Object(bucket_name, file_name)
     http = urllib3.PoolManager()
-
+    
+    
+    
     try:
         response = http.request('GET',
                                 "https://api.ukrainealarm.com/api/v3/alerts/regionHistory?regionId=16",
                                 headers={
                                     "accept": "application/json",
-                                    "authorization": "API_CODE"
+                                    "authorization": API_KEY
                                 },
                                 retries=False)
 
